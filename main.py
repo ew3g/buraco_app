6ef6b4e058c411db114730bb5d1afb2a5df7ac04
Edilson Rodrigues <edilson.w3g@gmail.com>
2023-05-05 22:52:29 -0300

    docs: jwt

--- main.py
+++ main.py
@@ -21,3 +21,7 @@ if __name__ == "__main__":
     uvicorn.run("main:app", host="127.0.0.1", port=8080,
                 log_level="info", reload=True)
     print("Running")
+
+
+# https://www.tutorialsbuddy.com/create-rest-api-to-perform-crud-operations-using-fastapi-and-mysql
+# https://testdriven.io/blog/fastapi-jwt-auth/
